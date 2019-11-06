a4b4cb46e8b8718bc152c68f60d0bfd141cf1fc4
wassil <work.wassdahi@gmail.com>
2019-11-06 19:41:03 +0100

    structured in functions

--- pdf-scraper.py
+++ pdf-scraper.py
@@ -5,18 +5,33 @@ import os
 import urllib
 import wget
 
-# connect to website and get list of all pdfs
+#import list of urls
 url=open("listurls.txt",'r')
 links_list=[]
-for l in url:
-    response = request.urlopen(l).read()
+
+def scraphtml(single_url):
+    print("ok")
+    response = request.urlopen(single_url).read()
     soup= BeautifulSoup(response, "html.parser")
     links_list.append( soup.find_all('a', href=re.compile(r'(.pdf)')))
-  
+    return links_list
 
-links=[]
-for i in links_list:
-    links.append(re.findall(r'http.+pdf',str(i)))
+def html_to_link(links_list):
+    links=[]
+    for i in links_list:
+        links.append(re.findall(r'http.+pdf',str(i)))
+    return links
 
-for l in links:
-   wget.download(l[0])
+def download_from_links(links):
+    for l in links:
+        wget.download(l)
+
+def final_download_single_link(single_url):
+    links_list=scraphtml(single_url)
+    links=html_to_link(links_list)
+    download_from_links(links)
+
+def final_download_multiple_link(list_urls):
+    for l in list_urls:
+        final_download_single_link(l)
+ 
